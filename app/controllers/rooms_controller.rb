6da966abba61cb7743def846966d6f6205bdc447
suzukimasatoshi-div <life6pride-h.m.t.r.hi.hi.hi@ezweb.ne.jp>
2021-07-19 06:31:57 +0900

    チャットルームの削除機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,7 @@
 class RoomsController < ApplicationController
+  
+  def index
+  end
 
   def new
     @room = Room.new
@@ -13,12 +16,15 @@ class RoomsController < ApplicationController
     end
   end
 
-  def index
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
   end
 
   private
 
   def room_params
-    params.require(:room).permit(:name, user_ids: [])
+    params.require(:room).permit(:name, user_ids:[])
   end
 end
